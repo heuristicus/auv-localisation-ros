cfd0917e6bc386037c71c70513f382ee0e856b6a
Michal Staniaszek <m.staniaszek@gmail.com>
2011-08-12 12:51:37 +0100

    removed unnecessary lines

--- nodes/extract_s.py
+++ nodes/extract_s.py
@@ -20,85 +20,64 @@ def callback(msgData):
  
     #Byte 17:
     headstatus = msgData[16]
-    hdstat = int(headstatus)
-    data.hdstat = hdstat
+    data.hdstat = int(headstatus)
     #print hdstat
     #print 'head status', headstatus
 
     #Byte 19,20:
     hdctrl1 = msgData[18]
-    hd1 = int(hdctrl1)
-    data.hd1 = hd1
+    data.hd1 = int(hdctrl1)
     #print 'HdCtrl bits (byte 1) - convert to binary and see manual for description:', hdctrl1
     hdctrl2 = msgData[19]
-    hd2 = int(hdctrl2)
-    data.hd2 = hd2
+    data.hd2 = int(hdctrl2)
     #print 'HdCtrl bits (byte 2) - convert to binary and see manual for description:', hdctrl2
    
     #Byte 21,22:
     rangescale = uint8_to_uint16([msgData[20], msgData[21]])
-    rng = rangescale
-    data.rng = float(rng)
-    #print 'range scale:', rangescale 
+    data.rng = rangescale
+    # print 'range scale:', rangescale 
 
     #Byte 34,35:
     ADInterval = uint8_to_uint16([msgData[33], msgData[34]])
-    ad = int(ADInterval)
-    data.ad = ad
+    data.ad = int(ADInterval)
     #print 'ADInterval:', ADInterval 
 
     #Byte 36,37:
     LeftLimit = uint8_to_uint16([msgData[35], msgData[36]])
-    llim = int(LeftLimit)
-    data.llim = llim
+    data.llim = int(LeftLimit)
     #print 'left limit of sonar sweep (1/16th gradian):', LeftLimit
 
     #Byte 38,39:
     RightLimit = uint8_to_uint16([msgData[37], msgData[38]])
-    rlim = int(RightLimit)
-    data.rlim = rlim
+    data.rlim = int(RightLimit)
     #print 'right limit of sonar sweep (1/16th gradian):', RightLimit
 
     #Byte 40:
     StepAngleSize = msgData[39]
-    step = int(StepAngleSize)
-    data.step = step
+    data.step = int(StepAngleSize)
     #print 'step size (between each ping) (1/16th gradian):', StepAngleSize
 
     #Bytes 41,42:
     transBearing = uint8_to_uint16([msgData[40], msgData[41]])
-    bear = int(transBearing)
-    data.bear = bear
+    data.bear = int(transBearing)
     #print 'bearing of transducer (1/16th gradian):', transBearing
     transBearingDeg = ((float(msgData[40]+(msgData[41]*256))/6400.0)*360)  #convert back from 16th of a gradian to degrees
-    transBearingDeg = transBearingDeg %360
-    beardeg = transBearingDeg
-    data.beardeg = beardeg
+    data.beardeg = transBearingDeg = transBearingDeg %360
     #print 'bearing of transducer (degrees):', transBearingDeg
 
     #Byte 43,44:
     nBins = uint8_to_uint16([msgData[42], msgData[43]])
-    nbins = int(nBins)
     data.nbins = nbins
     #print 'number of bins:', nBins
 
-    #data bytes:
-    #print 'bin data:', msgData[44:-1] 
-    bins = map(int, list(msgData[44:-1]))
-    data.bins = bins
-    #print bins
+    data.bins = map(int, list(msgData[44:-1]))
     pub.publish(data)
-    #print '_____________'
-
     
-    
-################################################################
 def uint8_to_uint16(input8):
     #converts two uint8s to one uint16
     output16 = input8[0]+(input8[1]*256)
     return output16
 
-################################################################
 def detectObstacle():
     global pub
     rospy.init_node('sonar_extractParameters', anonymous = True)
@@ -106,7 +85,6 @@ def detectObstacle():
     pub = rospy.Publisher('sonar_info', sonar_return)
     rospy.spin()
 
-################################################################
 if __name__ == '__main__':
     detectObstacle()
 
