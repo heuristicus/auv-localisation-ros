1b4e6882fec095bf72ca0ec19c791e62f5785f6f
Michal Staniaszek <m.staniaszek@gmail.com>
2011-08-15 13:09:22 +0100

    more updates to bag processing script

--- replay_bags.sh
+++ replay_bags.sh
@@ -1,22 +1,36 @@
-#!/bin/bash
+xparam=sonar/x
+yparam=sonar/y
+rparam=sonar/rng
+utp=/param_update
+umsg="update"
+
 
 for val in $(ls $1 | grep bag | awk 'BEGIN{FS=" "}{print $1}')
 
 do
-    rosbag info $1/$val >> bag_info.txt
-done
-
-echo `grep ERROR < bag_info.txt`
-
-exit
-
-do
-    echo $val
-    rosparam set sonar/x 10
-    rosparam set sonar/y 10
-    rosparam set sonar/rng 50
-    rostopic pub /param_update std_msgs/String "update" &
+    echo Checking filename $val for coordinate and range data...
+    [[ $val =~ micron_([0-9]+.[0-9]+)_([0-9]+.[0-9]+)_([0-9]+).bag ]]
     
-    rosbag play $1/$val
+    
+    if [ ! ${BASH_REMATCH} ]
+    then
+	echo No match, skipping file.
+    else
+	echo Match found...
+	x=${BASH_REMATCH[1]}
+	y=${BASH_REMATCH[2]}
+	rng=${BASH_REMATCH[3]}
+	echo Sending parameters to ros server...
+	echo $xparam = $x
+	echo $yparam = $y
+	echo $rparam = $rng
+	rosparam set $xparam $x
+	rosparam set $yparam $y
+	rosparam set $rparam $rng
+	echo Sending update notification to topic $utp
+	rostopic pub $utp std_msgs/String $umsg &
+	#rosbag play $1/$val
+    fi
+    
     sleep 2
 done
