8def55fe1b8014b612630506148ace04cc6c3251
Michal Staniaszek <m.staniaszek@gmail.com>
2011-08-15 11:57:50 +0100

    update to replay

--- nodes/process_sonar.py
+++ nodes/process_sonar.py
@@ -3,6 +3,7 @@ import roslib; roslib.load_manifest('loc_sonar')
 import rospy
 
 from loc_sonar.msg import sonar_return
+from std_msgs.msg import String
 
 global return_arr
 
@@ -15,13 +16,20 @@ def newdata(sonar):
     ind = int(round(sonar.beardeg))
     return_arr[ind].append(ind) # put ranges in here
 
+def update(val):
+    global x, y, rng
+    x = rospy.get_param('sonar/x')
+    y = rospy.get_param('sonar/y')
+    rng = rospy.get_param('sonar/rng')
+    print x,y,rng
+
 def get_distance(bins):
     """
     """
     threshold = 150
     blanking_dist = 0.3
     
-    bin_rng = pulse_range/len(bins)
+    bin_rng = rng/len(bins)
     
 
 
@@ -33,6 +41,7 @@ def main():
     return_arr = [[] for x in range(360)]
     rospy.init_node('process_sonar')
     sub = rospy.Subscriber('sonar_info', sonar_return, newdata)
+    sub = rospy.Subscriber('param_update', String, update)
     rospy.spin()
     
 if __name__ == '__main__':
--- replay_bags.sh
+++ replay_bags.sh
@@ -3,5 +3,20 @@
 for val in $(ls $1 | grep bag | awk 'BEGIN{FS=" "}{print $1}')
 
 do
+    rosbag info $1/$val >> bag_info.txt
+done
+
+echo `grep ERROR < bag_info.txt`
+
+exit
+
+do
     echo $val
+    rosparam set sonar/x 10
+    rosparam set sonar/y 10
+    rosparam set sonar/rng 50
+    rostopic pub /param_update std_msgs/String "update" &
+    
+    rosbag play $1/$val
+    sleep 2
 done
