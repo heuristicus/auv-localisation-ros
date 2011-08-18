bd136d3d26d97e9224cecfa8a150860ee37af0b5
Michal Staniaszek <m.staniaszek@gmail.com>
2011-08-18 09:13:01 +0100

    saving files works

--- nodes/process_sonar.py
+++ nodes/process_sonar.py
@@ -16,7 +16,7 @@ def newdata(sonar):
     #print bin_rng
     ind = int(round(sonar.beardeg))
     return_arr[ind].append(get_distance(sonar.bins)) # put ranges in here
-
+    
 def update(val):
     global x, y, rng
     save_array()
@@ -48,13 +48,28 @@ def get_first(arr):
     
 def save_array():
     global return_arr, x, y, rng
-    print return_arr
     if x is 0 or y is 0 or rng is 0:
         return
     else:
+        # average of the two ranges if there are for a certain angle
+        return_arr = [sum(z)/len(z) if len(z) >= 1 else z for z in return_arr]
         f = open('prc_%d_%d_%d.txt'%(x,y,rng), 'w')
-
+        f.write(string_arr(return_arr))
+        f.close()
+        
     return_arr = [[] for x in range(360)]
+
+def string_arr(array):
+    """
+    
+    Arguments:
+    - `array`:
+    """
+    cv = ''
+    for val in array:
+        cv += '%f '%(val) if val else '%d '%(0)
+    return cv[-1]
+
 
 def main():
     """
--- nodes/updateparams.py
+++ nodes/updateparams.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 import roslib; roslib.load_manifest('loc_sonar')
 import rospy
